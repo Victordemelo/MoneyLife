ca6a3edb89ff9c8eb67f862833ef14eff29096d3
Victor de Melo <victor.rosa.system@gmail.com>
2026-03-09 22:21:27 -0300

    style: Teste de view para rotas

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>user</title>
+    <title>principal</title>
 </head>
 <body>
-    <h1>teste user</h1>
+    <h1>Principal</h1>
     
 </body>
 </html>
