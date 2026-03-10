0d9be8be5c9b292b3a2a333a10c47234fbd10bab
Victor de Melo <victor.rosa.system@gmail.com>
2026-03-09 22:20:55 -0300

    Feat: ajuste das rotas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/painel', [UserController::class, 'index']) ->name('user.dashboard');
+Route::get('/admin', [AdminController::class, 'index']) ->name('admin.dashboard');
